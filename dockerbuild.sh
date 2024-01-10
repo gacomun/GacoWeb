dc85af39253aeb4c87a9ed90a296dab49c60e4b8
Javier <Javier@GACOINTELW.local>
2024-01-10 13:12:31 +0100

    Actualizar dockerbuild.sh, dockerstart.sh y requirements.txt

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,4 +1,3 @@
-#sudo docker build --rm -f Dockerfile -t gacomun/gacoweb:latest .
 sudo docker build --rm -f Dockerfile -t gacomun/gacoweb:latest .
 #sudo docker run --rm --privileged hypriot/qemu-register
 #sudo docker build --rm -f Dockerfilearm -t gacomun/gacoweb:latest-arm .
--- dockerstart.sh
+++ dockerstart.sh
@@ -1,6 +1,6 @@
 #sudo docker run --rm -d --name=gacomun_sonarr -p 9081:8081 gacomun/sickrage:latest
 #sudo docker run --rm -d --name=gacomun_sonarr -p 9081:8081 linuxserver/sickrage:latest
-sudo docker run --rm -d --name=gacomun_gacoweb -p 8020:8020 --env-file ./.env -v /home/javier/tmp/Gacoweb/data:/GacoWeb/data -v /home/javier/tmp/Gacoweb/log:/GacoWeb/log gacomun/gacoweb:latest
+sudo docker run --rm -d --name=gacomun_gacoweb -p 8020:8020 --env-file ./.env -v /home/javier/Descargas/docker/tmp/gacowebd/data:/GacoWeb/data -v //home/javier/Descargas/docker/tmp/gacowebd/log:/GacoWeb/log gacomun/gacoweb:latest
 #sudo docker run --rm -it --name=gacomun_gacoweb -p 8020:8020 -e DJANGO_SUPERUSER_USERNAME=javier -e DJANGO_SUPERUSER_PASSWORD=4058 -e DJANGO_SUPERUSER_EMAIL=admin@example.com gacomun/gacoweb:latest
 #sudo docker run -it --rm --name=gacomun_gacoweb gacomun/gacoweb:latest
 #sudo docker run -it --rm --name gacomun_sonarr gacomun/sickrage:latest
